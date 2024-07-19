da1c6041dec2b66a9a65e6fa8353421a8d6b20bf
Soham Karandikar <sohamk10@gmail.com>
2024-07-19 14:52:21 +0530

    Switch interrupt handler to use the C calling ABI. The x86-interrupt ABI did not store the first function argument in rdi, and I could not figure out where it's stored

--- src/interrupts/mod.rs
+++ src/interrupts/mod.rs
@@ -7,22 +7,38 @@ mod idt;
 lazy_static::lazy_static! {
     static ref IDT: idt::Idt = {
         let mut idt = idt::Idt::new();
-        idt.set_handler(InterruptType::DivideError, divide_error_handler);
+        idt.set_handler(InterruptType::DivideError, divide_error_wrapper);
         idt.set_handler(InterruptType::Breakpoint, breakpoint_handler);
         idt
     };
 }
-extern "x86-interrupt" fn divide_error_handler() -> ! {
-    let mut x: u64 = 0;
+
+#[naked]
+extern "C" fn divide_error_wrapper() -> ! {
     unsafe {
-        asm!("mov {}, rsp", out(reg) x);
-        let stack_frame = *(x as *const ExceptionStackFrame);
-        crate::println!("EXCEPTION: DIVIDE BY ZERO\n{:#?}", stack_frame);
+        asm!(
+            "
+            mov rdi, rsp
+            call {}
+            ",
+            sym divide_error_handler,
+            options(noreturn)
+        );
     }
+}
+
+extern "C" fn divide_error_handler(stack_frame: &ExceptionStackFrame) -> ! {
+    // crate::println!("Got frame as: {:#?}", stack_frame);
+    crate::println!("EXCEPTION: DIVIDE BY ZERO\n{:#?}", stack_frame);
+    // let mut x: u64 = 0;
+    // unsafe {
+    //     let stack_frame = *(x as *const exceptionstackframe);
+    //     crate::println!("exception: divide by zero\n{:#?}", stack_frame);
+    // }
     loop {}
 }
 
-extern "x86-interrupt" fn breakpoint_handler() -> ! {
+extern "C" fn breakpoint_handler() -> ! {
     crate::println!("EXCEPTION: BREAKPOINT");
     loop {}
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![no_main]
 #![feature(abi_x86_interrupt)]
+#![feature(naked_functions)]
 
 pub mod interrupts;
 pub mod serial;
